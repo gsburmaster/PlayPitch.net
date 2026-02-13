41e8db3463ea913a2c48d2063fade82ee3fa84f5
gsburmaster <gsburmaster@gmail.com>
2026-02-12 21:44:19 -0600

    cleanup

--- webserver/dist/routes/rooms.d.ts
+++
@@ -1,3 +0,0 @@
-import { Router } from "express";
-import type { RoomManager } from "../rooms/RoomManager.js";
-export declare function createRoomRoutes(roomManager: RoomManager): Router;
--- webserver/dist/ws/handler.d.ts
+++
@@ -1,3 +0,0 @@
-import type { WebSocket } from "ws";
-import type { RoomManager } from "../rooms/RoomManager.js";
-export declare function handleWebSocket(ws: WebSocket, roomManager: RoomManager): void;
